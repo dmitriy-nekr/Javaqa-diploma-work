869ba60bd61903007afdde2f75040da693537096
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-12-07 18:43:02 +0300

    Initial coomit

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -8,14 +8,75 @@ import org.junit.jupiter.api.Test;
 public class GameStoreTest {
 
 
-        @Test
-        public void shouldAddGame() {
+    @Test
+    public void shouldAddGame() {
 
-            GameStore store = new GameStore();
-            Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
-            assertTrue(store.containsGame(game));
-        }
-
-        // другие ваши тесты
+        assertTrue(store.containsGame(game));
     }
+
+    @Test
+    public void shouldVerifyNotAddGame() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game =new Game("Игра 1", "Аркады",store);
+        assertFalse(store.containsGame(game));
+    }
+
+    @Test
+    public void shouldAddPlayerWithPlayTime1() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", 1);
+        store.addPlayTime("Kolya", 0);
+        String expected = "Ivan";
+        String actual = store.getMostPlayer();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldAddPlayerWithPlayTime2() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", 5);
+        store.addPlayTime("Kolya", 4);
+        store.addPlayTime("Kolya", 3);
+        String expected = "Kolya";
+        String actual = store.getMostPlayer();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldAddPlayerWithPlayTime0() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", 0);
+        store.addPlayTime("Kolya", 0);
+        store.addPlayTime("Kolya", 0);
+        String expected = null;
+        String actual = store.getMostPlayer();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldSumPlayedTime() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", 5);
+        store.addPlayTime("Kolya", 6);
+        store.addPlayTime("Ivan", 5);
+        int expected = 16;
+        int actual = store.getSumPlayedTime();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldSumWithPlayTime0() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Ivan", 0);
+        store.addPlayTime("Kolya", 0);
+        store.addPlayTime("Kolya", 0);
+        String expected = null;
+        String actual = store.getMostPlayer();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // другие ваши тесты
+}
