b6898ccc1478ee66901b45abe2325ca576460a95
Nekrasov Dmitriy <dimannekr@mail.ru>
2022-12-07 14:14:50 +0300

    Add tests for Player class

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
-
-
+    GameStore store = new GameStore();
+    Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+    Game game1= store.publishGame("Варкрафт", "Аркады");
+    Game game2= store.publishGame("Стрелялка", "Шутеры");
+    Player player = new Player("Petya");
 
     @Test
     public void shouldSumGenreIfOneGame() {
-        GameStore store = new GameStore();
-        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
-        Player player = new Player("Petya");
         player.installGame(game);
         player.play(game, 3);
 
@@ -22,8 +22,69 @@ public class PlayerTest {
         int actual = player.sumGenre(game.getGenre());
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    public void shouldSumGenreIfTwoGame() {
+
+        player.installGame(game);
+        player.installGame(game1);
 
 
+        player.play(game, 3);
+        player.play(game1, 6);
+
+
+        int expected = 9;
+        int actual = player.sumGenre(game.getGenre());
+        Assertions.assertEquals(expected, actual);
     }
-        // другие ваши тесты
+    @Test
+    public void shouldSumGenreIfOneGameHasAnotherGenre() {
+
+        player.installGame(game);
+        player.installGame(game1);
+        player.installGame(game2);
+
+        player.play(game, 3);
+        player.play(game1, 6);
+        player.play(game2, 4);
+
+        int expected = 9;
+        int actual = player.sumGenre(game.getGenre());
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldGetException() {
+
+        Assertions.assertThrows(RuntimeException.class, () -> player.play(game, 3));
+    }
+
+    @Test
+    public void shouldGetMostPlayedGame(){
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 3);
+        player.play(game1, 6);
+
+        Game expected=game1;
+        Game actual = player.mostPlayerByGenre("Аркады");
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldGetMostPlayedGameWhichNotPlayed(){
+        player.installGame(game);
+        player.installGame(game1);
+        player.installGame(game2);
+        player.play(game, 3);
+        player.play(game1, 6);
+
+        Game expected=null;
+        Game actual = player.mostPlayerByGenre("Шутеры");
+        Assertions.assertEquals(expected,actual);
+    }
+
+
+}
+// другие ваши тесты
 
